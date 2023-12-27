a0e9bf576f88b7aa5db533717f5fe01ac5fb6625
Ed <esmartqq@gmail.com>
2023-12-27 15:07:50 +0800

    fix find words function, print HSK breakdown to console

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,12 +19,29 @@ public class Main {
         }
       }
       SubtitleComparator subComp = new SubtitleComparator(chineseSentences);
+      String textBreakdown = formatHSKBreakdown(subComp.getHSKCounts());
+      System.out.println(textBreakdown);
     } else {
       System.out.println("Usage: hsk-grader [file-path]");
     }
   }
 
-  public static boolean containsHanScript(String s) {
+  private static String formatHSKBreakdown(Hashtable<String, Integer> breakdown) {
+    String textBreakdown = "HSK Breakdown - \n";
+    double total = 0;
+    for (int i = 1; i < 10; i++) {
+      double percentage = (double)breakdown.get(String.valueOf(i))/(double)breakdown.get("total")*100;
+      textBreakdown += "HSK" + String.valueOf(i) + ": " + String.valueOf(percentage) + "%\n";
+      total += percentage;
+    }
+    double unknownPercentage = (double)breakdown.get("unknown")/(double)breakdown.get("total")*100;
+    total += unknownPercentage;
+    System.out.println(String.valueOf(total));
+    textBreakdown += "Unknown: " + String.valueOf(unknownPercentage) + "%";
+    return textBreakdown; 
+  }
+
+  private static boolean containsHanScript(String s) {
     return s.codePoints().anyMatch(
       codepoint ->
       Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN);
--- src/SubtitleComparator.java
+++ src/SubtitleComparator.java
@@ -4,27 +4,37 @@ import java.net.URL;
 
 public class SubtitleComparator {
   private List<String> subtitleLines;
-  private Hashtable<String, String> chineseWordsWithHSK;
+  private Hashtable<String, String> chineseHSKReferenceDict;
   private Hashtable<String, String> wordsInSubtitles;
+  private Hashtable<String, Integer> HSKCounts;
   
   public SubtitleComparator(List<String> userSubtitles) throws FileNotFoundException {
     subtitleLines = userSubtitles; 
-    chineseWordsWithHSK = populateChineseWordsWithHSK();
+    chineseHSKReferenceDict = populateChineseHSKReferenceDict();
+    HSKCounts = initHSKCounts();
     wordsInSubtitles = findWords(subtitleLines);
   }
 
-  private Hashtable<String, String> populateChineseWordsWithHSK() throws FileNotFoundException {
+  private Hashtable<String, Integer> initHSKCounts() {
+    Hashtable<String, Integer> counts = new Hashtable<String, Integer>();
+    for (int i = 1; i < 10; i++) {
+      counts.put(String.valueOf(i), 0);
+    }
+    counts.put("unknown", 0);
+    counts.put("total", 0);
+    return counts;
+  }
+
+  private Hashtable<String, String> populateChineseHSKReferenceDict() throws FileNotFoundException {
     Hashtable<String, String> wordsHSK = new Hashtable<String, String>();
-    URL url = getClass().getResource("dict_hsk.csv");
-    File input = new File(url.getPath());
-    try (Scanner myReader = new Scanner(input)) {
+    InputStream inputStream = getClass().getResourceAsStream("/dict_hsk.csv");
+    try (Scanner myReader = new Scanner(inputStream)) {
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
         String[] splitData = data.split(",");
         wordsHSK.put(splitData[0],splitData[1]);
       }
     }
-    System.out.println(wordsHSK.toString());
     return wordsHSK;
   }
 
@@ -35,15 +45,22 @@ public class SubtitleComparator {
       for (String textBlock : textBlocks) {
         char[] chineseChars = textBlock.toCharArray();
         while (chineseChars.length > 0) {
-          for (int i = textBlocks.length - 1; i <= 0; i--) {
+          for (int i = chineseChars.length; i > 0; i--) {
             char[] checkChars = Arrays.copyOfRange(chineseChars, 0, i);
             String wordCandidate = String.valueOf(checkChars);
-            if (chineseWordsWithHSK.containsKey(wordCandidate)) {
-              subtitleWords.put(wordCandidate,chineseWordsWithHSK.get(wordCandidate));
+            if (chineseHSKReferenceDict.containsKey(wordCandidate)) {
+              String HSKLevel = chineseHSKReferenceDict.get(wordCandidate);
+              subtitleWords.put(wordCandidate,HSKLevel);
+              HSKCounts.put(HSKLevel, HSKCounts.get(HSKLevel) +1);
+              HSKCounts.put("total", HSKCounts.get("total") + 1);
               chineseChars = Arrays.copyOfRange(chineseChars, i, chineseChars.length);
+                break;
+              } else if (i == 1) {
+                subtitleWords.put(wordCandidate,"unknown");
+              HSKCounts.put("unknown",HSKCounts.get("unknown") + 1);
+              HSKCounts.put("total", HSKCounts.get("total") + 1);
+              chineseChars = new char[0];
               break;
-            } else if (i == 1) {
-              subtitleWords.put(wordCandidate,"unknown");
             } else {
               continue;
             }
@@ -52,5 +69,9 @@ public class SubtitleComparator {
       }
     }
     return subtitleWords;
+  }
+
+  public Hashtable<String, Integer> getHSKCounts() {
+    return HSKCounts;
   }
 }
